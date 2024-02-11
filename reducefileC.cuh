84b7f0754f695d9bd9c28eea144f4c4a5d315e42
mahtab <mahtabnejadt@gmail.com>
2024-02-11 03:30:18 +0330

    add lots of gpuErrchk function calls to make sure everything is synchronized and wait for all threads to be executed

--- reducefileC.cuh
+++ reducefileC.cuh
@@ -326,28 +326,70 @@ int *zerofactorrsumblock1,int *zerofactorrsumblock2,int *zerofactorrsumblock3,in
     reduceTraj<<<grid_size, blockSize>>>(d_x, d_y, d_z, d_xx, d_yy, d_zz, N, skipfactor, roundedNumber_x, roundedNumber_y, roundedNumber_z, zerofactorr);
 
     initializeArray<<<grid_size, blockSize>>>(d_xx_lim1, NN, 1000.000000);
+    gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_xx_lim2, NN, 1000.000000);
+    gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_xx_lim3, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_xx_lim4, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_xx_lim5, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_xx_lim6, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_xx_lim7, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
 
     initializeArray<<<grid_size, blockSize>>>(d_yy_lim1, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_yy_lim2, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_yy_lim3, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_yy_lim4, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_yy_lim5, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_yy_lim6, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_yy_lim7, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
 
     initializeArray<<<grid_size, blockSize>>>(d_zz_lim1, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_zz_lim2, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_zz_lim3, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_zz_lim4, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_zz_lim5, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_zz_lim6, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_zz_lim7, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
 
     spatial_limiting_kernel<<<grid_size, blockSize>>>(d_xx, d_yy, d_zz,
         d_xx_lim1, d_yy_lim1, d_zz_lim1, zerofactorr1,
@@ -363,15 +405,28 @@ int *zerofactorrsumblock1,int *zerofactorrsumblock2,int *zerofactorrsumblock3,in
 
     //in this line we should sum over all zerofactorr elements to calculate zerofactorr_sum
     intreduceKernel_<<<grid_size_,blockSize_,shared_mem_size_>>>(zerofactorr, zerofactorrsumblock, N);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
 
     intreduceKernel_<<<grid_size_,blockSize_,shared_mem_size_>>>(zerofactorr1, zerofactorrsumblock1, N);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     intreduceKernel_<<<grid_size_,blockSize_,shared_mem_size_>>>(zerofactorr2, zerofactorrsumblock2, N);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     intreduceKernel_<<<grid_size_,blockSize_,shared_mem_size_>>>(zerofactorr3, zerofactorrsumblock3, N);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     intreduceKernel_<<<grid_size_,blockSize_,shared_mem_size_>>>(zerofactorr4, zerofactorrsumblock4, N);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     intreduceKernel_<<<grid_size_,blockSize_,shared_mem_size_>>>(zerofactorr5, zerofactorrsumblock5, N);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     intreduceKernel_<<<grid_size_,blockSize_,shared_mem_size_>>>(zerofactorr6, zerofactorrsumblock6, N);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     intreduceKernel_<<<grid_size_,blockSize_,shared_mem_size_>>>(zerofactorr7, zerofactorrsumblock7, N);
-
     gpuErrchk( cudaPeekAtLastError() );
     gpuErrchk( cudaDeviceSynchronize() );
 
@@ -533,31 +588,74 @@ __host__ void reducevel(std::string basename, double *d_vx,double *d_vy, double 
     int block_sum_zerofactor7[grid_size_];
 
     reduceVel<<<grid_size, blockSize>>>(d_vx, d_vy, d_vz, d_vxx, d_vyy, d_vzz, d_x, d_y, d_z, N, skipfactor, roundedNumber_vx, roundedNumber_vy, roundedNumber_vz, zerofactor);
-
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
 
     initializeArray<<<grid_size, blockSize>>>(d_vxx_lim1, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_vxx_lim2, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_vxx_lim3, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_vxx_lim4, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_vxx_lim5, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_vxx_lim6, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_vxx_lim7, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
 
     initializeArray<<<grid_size, blockSize>>>(d_vyy_lim1, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_vyy_lim2, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_vyy_lim3, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_vyy_lim4, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_vyy_lim5, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_vyy_lim6, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_vyy_lim7, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
 
     initializeArray<<<grid_size, blockSize>>>(d_vzz_lim1, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_vzz_lim2, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_vzz_lim3, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_vzz_lim4, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_vzz_lim5, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_vzz_lim6, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     initializeArray<<<grid_size, blockSize>>>(d_vzz_lim7, NN, 1000.000000);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
 
 
     velocity_limiting_kernel<<<grid_size, blockSize>>>(d_vxx, d_vyy, d_vzz,d_x, d_y, d_z,
@@ -569,17 +667,34 @@ __host__ void reducevel(std::string basename, double *d_vx,double *d_vy, double 
         d_vxx_lim6, d_vyy_lim6, d_vzz_lim6,  zerofactor6,
         d_vxx_lim7, d_vyy_lim7, d_vzz_lim7,  zerofactor7, NN);
 
-
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     //in this line we should sum over all zerofactor elements to calculate zerofactor_sum
     intreduceKernel_<<<grid_size_,blockSize_,shared_mem_size_>>>(zerofactor, zerofactorsumblock, N);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
 
     intreduceKernel_<<<grid_size_,blockSize_,shared_mem_size_>>>(zerofactor1, zerofactorsumblock1, N);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     intreduceKernel_<<<grid_size_,blockSize_,shared_mem_size_>>>(zerofactor2, zerofactorsumblock2, N);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     intreduceKernel_<<<grid_size_,blockSize_,shared_mem_size_>>>(zerofactor3, zerofactorsumblock3, N);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     intreduceKernel_<<<grid_size_,blockSize_,shared_mem_size_>>>(zerofactor4, zerofactorsumblock4, N);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     intreduceKernel_<<<grid_size_,blockSize_,shared_mem_size_>>>(zerofactor5, zerofactorsumblock5, N);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
     intreduceKernel_<<<grid_size_,blockSize_,shared_mem_size_>>>(zerofactor6, zerofactorsumblock6, N);
-    intreduceKernel_<<<grid_size_,blockSize_,shared_mem_size_>>>(zerofactor7, zerofactorsumblock7, N); 
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() );
+    intreduceKernel_<<<grid_size_,blockSize_,shared_mem_size_>>>(zerofactor7, zerofactorsumblock7, N);
+     gpuErrchk( cudaPeekAtLastError() );
+    gpuErrchk( cudaDeviceSynchronize() ); 
 
     cudaMemcpy(block_sum_zerofactor, zerofactorsumblock, grid_size_*sizeof(int), cudaMemcpyDeviceToHost);
 
