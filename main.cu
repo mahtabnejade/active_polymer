7ea862370dda7b5cd80c42a5c7582079608ed100
mahtab <mahtabnejadt@gmail.com>
2024-03-12 02:09:15 +0330

    add d_vy d_vz d_mdVy d_mdVz in sort_finish function call

--- main.cu
+++ main.cu
@@ -499,8 +499,8 @@ int main(int argc, const char* argv[])
                     d_ux , d_uy , d_uz , d_e , d_scalefactor , d_n , d_m ,
                     d_rot , d_theta , d_phi , N , Nmd ,Nc ,devStates , grid_size);
             
-                Sort_finish(d_x , d_y , d_z ,d_vx, d_index , 
-                    d_mdX , d_mdY , d_mdZ ,d_mdVx, d_mdIndex ,ux , 
+                Sort_finish(d_x , d_y , d_z , d_vx, d_vy, d_vz, d_index , 
+                    d_mdX , d_mdY , d_mdZ, d_mdVx, d_mdVy, d_mdVz, d_mdIndex, ux, 
                     d_L , d_r , N , Nmd , real_time, grid_size);
             
                 real_time += h_mpcd;
