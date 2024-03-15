43a93ccc275d74ae0c912ba5896999320085f2c6
mahtab <mahtabnejadt@gmail.com>
2024-03-16 00:50:46 +0330

    call XL function to make sure whenever particles go out of boxes due to errors, we keep them still on the walls

--- nonslipXperiodic.cuh
+++ nonslipXperiodic.cuh
@@ -45,6 +45,23 @@ __device__ double symmetric_heaviside_right(double x, double L){
 
 }
 
+__device__ double XL(double x, double L){
+
+    if (x > L)
+        return L;
+    else  if (x == L)
+        return L;
+    else if (-L < x < L)
+        return x;
+    else 
+        return -L;
+    
+
+}
+
+
+
+
 __global__ void nonslipXperiodicBC1(double *x1 ,double *x2 , double *x3, double *v1 ,double *v2, double *v3, double ux,double *L, double t, int N)
 {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
@@ -89,7 +106,7 @@ _global__ void nonslipXperiodicBC2(double *x1 ,double *x2 , double *x3, double *
             v1[tid] *= ((symmetric_heaviside_left(x2[tid],L[1]/2)-symmetric_heaviside_right(x2[tid],-L[1]/2)) - 1);// vx in y plane (in cube sides should be multipid by -1 but elsewhere must be multipied by 1)
             //we could also go like this:
             //v1[tid] *= (2*(heaviside_left(x2[tid],L[1]/2)-heaviside_right(x2[tid],-L[1]/2)))-1);
-            
+
             v3[tid] *= ((symmetric_heaviside_left(x2[tid],L[1]/2)-symmetric_heaviside_right(x2[tid],-L[1]/2)) - 1);// vz in y plane  (in cube sides should be multipid by -1but elsewhere must be multipied by 1 )
             
             v1[tid] *= ((symmetric_heaviside_left(x3[tid],L[2]/2)-symmetric_heaviside_right(x3[tid],-L[2]/2)) - 1);// vx in z plane (in cube sides should be multipid by -1 but elsewhere must be multipied by 1)
@@ -115,19 +132,26 @@ _global__ void nonslipXperiodicBC3(double *x1 ,double *x2 , double *x3, double *
             //use the heaviside_right and heaviside_left functions in nonslipXperiodicBC kernel.
 
             v2[tid] *= (heaviside_left(x2[tid],L[1]/2)-heaviside_right(x2[tid],-L[1]/2));// vy in y plane (in cube sides must be zero but elsewhere must be multipied by 1 )
-            v3[tid] *= (heaviside_left(x3[tid],L[2]/2)-heaviside_right(x3[tid],-L[2]/2));// vz in z plane (in cube sides must be zero but elsewhere must be multipied by 1 )
-           
-           
             v1[tid] *= ((heaviside_left(x2[tid],L[1]/2)-heaviside_right(x2[tid],-L[1]/2)) );// vx in y plane (in cube sides must be zero but elsewhere must be multipied by 1)
             v3[tid] *= ((heaviside_left(x2[tid],L[1]/2)-heaviside_right(x2[tid],-L[1]/2)) );// vz in y plane  (in cube sides must be zero but elsewhere must be multipied by 1 )
             
             v1[tid] *= ((heaviside_left(x3[tid],L[2]/2)-heaviside_right(x3[tid],-L[2]/2)) );// vx in z plane (in cube sides must be zero but elsewhere must be multipied by 1)
             v2[tid] *= ((heaviside_left(x3[tid],L[2]/2)-heaviside_right(x3[tid],-L[2]/2)) );// vy in z plane (in cube sides must be zero but elsewhere must be multipied by 1)
+            v3[tid] *= (heaviside_left(x3[tid],L[2]/2)-heaviside_right(x3[tid],-L[2]/2));// vz in z plane (in cube sides must be zero but elsewhere must be multipied by 1 )
+
 
            //we keep the x plane periodic still. 
             x1[tid] -= ux * t * round(x3[tid] / L[2]);
             x1[tid] -= L[0] * (round(x1[tid] / L[0]));
             v1[tid] -= ux * round(x3[tid] / L[2]);
+
+        //we should make sure at the same time the particles are exactly on the walls when they velocities become zero
+            x2[tid] = XL(x2[tid] , L[1]/2);
+            x3[tid] = XL(x3[tid] , L[2]/2);
+
+            x2[tid] = 
+            x3[tid] =  
+
             
     }
 }
