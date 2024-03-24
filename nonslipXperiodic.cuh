5c00c80c3320ab324c112d82af7623553ddce4f3
mahtab <mahtabnejadt@gmail.com>
2024-03-24 16:58:23 +0330

    add some printf statements in the code to check where the error happens

--- nonslipXperiodic.cuh
+++ nonslipXperiodic.cuh
@@ -151,9 +151,9 @@ __global__ void nonslipXperiodicBC3(double *x1 ,double *x2 , double *x3, double 
             //if (((heaviside_left(x2[tid],(-L[1]/2 + epsilon))-heaviside_right(x2[tid],(L[1]/2 - epsilon))) ) == 0)  printf("  x2[tid] = %f\n", x2[tid]);
             //if (x2[tid]>1000) printf("x2[tid] = %f, tid = %i\n", x2[tid], tid);
             //if (x3[tid]>1000) printf("x3[tid] = %f, tid = %i\n", x3[tid], tid);
-            printf("x2[0]=%f\n", x2[0]);
-            if (tid == 0 && x2[tid] >1000) printf("x2[0] = %f\n", x2[0]); 
-            if (tid != 0 && x2[tid] >1000) printf("error"); 
+            if (tid == 0) printf("x2[0]=%f\n", x2[0]);
+            //if (tid == 0 && x2[tid] >1000) printf("x2[0] = %f\n", x2[0]); 
+            if (tid != 0 && x2[tid] >40) printf("error x2[tid]=%f , tid=%i\n", x2[tid] , tid); 
             //use the heaviside_right and heaviside_left functions in nonslipXperiodicBC kernel.
 
             v2[tid] *= ((heaviside_left(x2[tid],(-L[1]/2 + epsilon))-heaviside_right(x2[tid],(L[1]/2 - epsilon))));// vy in y plane (in cube sides must be zero but elsewhere must be multipied by 1 )
